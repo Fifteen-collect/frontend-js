0005d6c59fedec69ad65a938a26e6c626cfdb390
user-rq <roman.varkuta@gmail.com>
2019-11-25 14:09:37 +0200

    Fix build names for chunks

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ module.exports = {
     output: {
         path: path.join(__dirname, "/"),
         filename: "bundle.js",
+        chunkFilename: './[name].bundle.js'
     },
     resolve: {
         modules: [

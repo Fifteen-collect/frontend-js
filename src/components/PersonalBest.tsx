ac91e14abfe0e2a8e57b4be05c63f85d3086e793
user-rq <roman.varkuta@gmail.com>
2019-11-27 20:13:54 +0200

    Service for work with the best results

--- src/components/PersonalBest.tsx
+++ src/components/PersonalBest.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
 import * as PropTypes from "prop-types";
 import {Size} from "types/ColorScheme";
-import Bar from "components/Bar";
+import {PersonalBestInterface} from "interfaces/PersonalBestInterface";
+import PersonalBestService from "./Service/PersonalBestService";
 
 export interface PersonalBestProps {
     size: Size
@@ -11,30 +12,22 @@ export const PersonalBestPropTypes: { [T in keyof PersonalBestProps]: PropTypes.
     size: PropTypes.number,
 };
 
-export interface PersonalBestState {
-    time?: number,
-    tps?: number,
-    moves?: number,
-    scramble?: Bar[][],
-}
-
-export class PersonalBest extends React.Component<PersonalBestProps, PersonalBestState> {
+export class PersonalBest extends React.Component<PersonalBestProps, PersonalBestInterface> {
     public static readonly propTypes = PersonalBestPropTypes;
 
     constructor(props: PersonalBestProps) {
         super(props);
 
-        const pb = localStorage.getItem(this.props.size.toString(10));
-        this.state = pb ? JSON.parse(pb) : {};
+        this.state = PersonalBestService.getStats(props.size) || {};
     }
 
     public render() {
         return <div className="container-fluid">
             <div className="row">
-                <div className="col-3">Personal bests: </div>
-                <div className="col-3">[Time: {this.state.time || '-'}] </div>
-                <div className="col-3">[Tps: {this.state.tps || '-'}] </div>
-                <div className="col-3">[Moves: {this.state.moves || '-'}] </div>
+                <div className="col-3 p-0">Pb: </div>
+                <div className="col-3 p-0">Time: {this.state.time || '-'} </div>
+                <div className="col-3 p-0">Tps: {this.state.tps || '-'} </div>
+                <div className="col-3 p-0">Moves: {this.state.moves || '-'} </div>
             </div>
         </div>
     }
---
+++ src/components/Service/PersonalBestService.ts
@@ -0,0 +1,27 @@
+import {Size} from "types/ColorScheme";
+import {PersonalBestInterface} from "interfaces/PersonalBestInterface";
+
+export default class PersonalBestService {
+    public static getStats(size: Size): PersonalBestInterface | null {
+        return JSON.parse(
+            localStorage.getItem(
+                size.toString(10)
+            )
+        );
+    }
+
+    public static saveStats(size: Size, stats: PersonalBestInterface): boolean {
+        try {
+            localStorage.setItem(
+                size.toString(10),
+                JSON.stringify(stats)
+            );
+        } catch (e) {
+            console.error(e);
+
+            return false;
+        }
+
+        return true;
+    }
+}

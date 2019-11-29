5e47df09da3c4a3029e23730701b6b0263ceb525
KartaviK <varkuta.roma@gmail.com>
2019-11-29 09:46:59 +0200

    Update polyfills dependencies

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import * as ReactDOM from 'react-dom';
 import App from './Components/App';
 import 'bootstrap/dist/css/bootstrap.css';
 import './Styles/main.css';
-import 'core-js';
-import 'raf/polyfill';
-import 'intl';
+import './polyfills';
 
 if (!window.Intl) {
     require.ensure([], () => {

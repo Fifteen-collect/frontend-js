6efe3c4f9268d36b01ee84c8984ae2497d4959b3
Kartav1K <roman.varkuta@gmail.com>
2019-12-12 23:43:32 +0200

    New build with integrated sw

---
+++ precache-manifest.ffa8a3416874281ee617374091946c10.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9c11be3f50fa8650604",
+    "url": "bundle.js"
+  },
+  {
+    "revision": "b45b4dc5fc10a61ecb4c41f4fb82522f",
+    "url": "index.html"
+  }
+]);
